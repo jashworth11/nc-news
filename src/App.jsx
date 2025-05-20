7766b902b2292d54d3d5609e440372a0eeef0ec5
Joe Ashworth <joeashworth3427@gmail.com>
2025-05-20 15:51:58 +0100

    task 8 done, added home page and login so only logged in user can post comment

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,24 @@ import { NavBar } from "./components/NavBar";
 import { ArticlesList } from "./components/ArticleList";
 import { Header } from "./components/Header";
 import { SingleArticle } from "./components/SingleArticle";
+import { useState } from "react";
+import { Login } from "./components/Login";
+import { HomePage } from "./components/HomePage";
 
 function App() {
+  const [user, setUser] = useState(null);
   return (
     <>
-      <NavBar />
+      <NavBar user={user} />
       <Header />
       <Routes>
-        <Route path="/" element={<ArticlesList />} />
+        <Route path="/" element={<HomePage user={user} />} />
         <Route path="/articles" element={<ArticlesList />} />
-        <Route path="/articles/:article_id" element={<SingleArticle />} />
+        <Route
+          path="/articles/:article_id"
+          element={<SingleArticle user={user} />}
+        />
+        <Route path="/login" element={<Login setUser={setUser} />} />{" "}
       </Routes>
     </>
   );
--- src/components/CommentList.jsx
+++ src/components/CommentList.jsx
@@ -2,8 +2,9 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getCommentsByArticleId } from "../../api";
 import { CommentCard } from "./CommentCard";
+import { CommentForm } from "./CommentForm";
 
-export const CommentList = () => {
+export const CommentList = ({ user }) => {
   const { article_id } = useParams();
   const [comments, setComments] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
@@ -15,11 +16,15 @@ export const CommentList = () => {
         setComments(response.data.comments);
         setIsLoading(false);
       })
-      .catch((err) => {
+      .catch(() => {
         setError("Failed to load comments.");
         setIsLoading(false);
       });
   }, [article_id]);
+
+  const handleNewComment = (newComment) => {
+    setComments((current) => [newComment, ...current]);
+  };
 
   if (isLoading) return <p>Loading comments...</p>;
   if (error) return <p>{error}</p>;
@@ -27,6 +32,7 @@ export const CommentList = () => {
   return (
     <div className="comment-list">
       <h3>Comments</h3>
+      <CommentForm user={user} onAddComment={handleNewComment} />
       {comments.length === 0 ? (
         <p>No comments yet. Be the first to comment!</p>
       ) : (
