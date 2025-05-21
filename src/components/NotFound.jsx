0f39e8a34cbc35faa2acaf9ddde17cb7bdda6e3d
Joe Ashworth <joeashworth3427@gmail.com>
2025-05-21 14:20:54 +0100

    error handling

---
+++ src/components/NotFound.jsx
@@ -0,0 +1,8 @@
+export const NotFound = () => {
+  return (
+    <main style={{ textAlign: "center", marginTop: "3rem" }}>
+      <h2>404 - Page Not Found</h2>
+      <p>Sorry, the page you're looking for doesn't exist.</p>
+    </main>
+  );
+};
